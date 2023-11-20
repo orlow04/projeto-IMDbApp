93e1a8dee73a10cf9a14a9dc320b7621386d7488
Gabriel Orlow <gabrielorlow@discente.ufg.br>
2023-11-20 17:12:44 -0300

    Update classe-donocinema.py

--- classe-donocinema.py
+++ classe-donocinema.py
@@ -1,2 +1,41 @@
-class donoCinema(Cliente)
+class donoCinema(Cliente):
+  def __init__(self,tipoCliente,cpf,empresa,endereco,valor):
+    super().__init__(tipoCliente)
+    self.__cpf = cpf
+    self.__empresa = empresa
+    self.__endereco = endereco
+    self.__valor = valor
+
+  @property
+  def cpf(self):
+    return self.__cpf
+
+  @cpf.setter
+  def cpf(self,cpf):
+    self.__cpf = cpf
+
+  @property
+  def empresa(self):
+    return self.__empresa
+
+  @empresa.setter
+  def empresa(self,empresa):
+    self.__empresa = empresa
+
+  @property
+  def endereco(self):
+    return self.__endereco
+
+  @endereco.setter
+  def endereco(self,endereco):
+    self.__endereco = endereco
+
+  @property
+  def valor(self):
+    return self.__valor
+
+  @valor.setter
+  def valor(self,valor):
+    self.__valor = valor
+    
 
