408f3c05646960cb381d838eafe715408d40d60c
Gabriel Orlow <gabrielorlow@discente.ufg.br>
2023-11-20 16:57:21 -0300

    Update classe-genero.py

--- classe-genero.py
+++ classe-genero.py
@@ -1,18 +1,13 @@
 class Genero:
   def __init__(self,tipo)
     self.__tipo = tipo
-    self.__filme = None
-
 
   @property
   def tipo(self):
     return self.__tipo
 
-  def genero(self):
-    return self.__genero
-
-  @filme.setter
-  def filme(self,genero):
-    self.__filme = genero
+  @tipo.setter
+  def tipo(self,tipo):
+    self.__tipo = tipo
 
   
