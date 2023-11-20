c92ab84de149370b67551fe2a2b64faba93dd0e9
matheussfm <134388612+matheussfm@users.noreply.github.com>
2023-11-20 19:45:40 -0300

    classe-diretor.py

--- classe-diretor.py
+++ classe-diretor.py
@@ -1,1 +1,7 @@
-
+class Diretor: 
+  def __init__(self, nome, outros_trabalhos, main, biografia):
+    self.nome = nome
+    self.outros_trabalhos = outros_trabalhos
+    self.main = main
+    self.biografia = biografia
+    
