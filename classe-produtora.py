968091a63f626f5362a637e0fb3a1611a430fa0a
Gabriel Orlow <gabrielorlow@discente.ufg.br>
2023-11-20 18:31:35 -0300

    Update classe-produtora.py

--- classe-produtora.py
+++ classe-produtora.py
@@ -1,1 +1,20 @@
+class Produtora:
+  def __init__(self,nome,pais):
+    self.__nome = nome
+    self.__pais = pais
 
+  @property
+  def nome(self):
+    return self.__nome
+
+  @nome.setter
+  def nome(self,nome):
+    self.__nome = nome
+
+  @property
+  def pais(self):
+    return self.__pais
+
+  @pais.setter
+  def pais(self,pais):
+    self.__pais = pais
