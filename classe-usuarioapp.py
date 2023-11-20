4aeeb6b67fca7d518fc0ec1e7d5cebe27fc3d70e
GustavoFernandesdaCunha <136812261+GustavoFernandesdaCunha@users.noreply.github.com>
2023-11-20 19:48:33 -0300

    Update classe-usuarioapp.py

--- classe-usuarioapp.py
+++ classe-usuarioapp.py
@@ -1,1 +1,40 @@
+class usuarioApp(Cliente):
+  def __init__(self,tipoCliente,idUsuario,perfil,historico,favorito):
+    super().__init__(tipoCliente)
+    self.__idUsuario = idUsuario
+    self.__perfil = perfil
+    self.__historico = historico
+    self.__favorito = favorito
 
+@property
+  def idUsuario(self):
+    return self.__idUsuario
+
+  @cpf.setter
+  def idUsuario(self,idUsuario):
+    self.__idUsuario = idUsuario
+
+  @property
+  def perfil(self):
+    return self.__perfil
+
+  @empresa.setter
+  def perfil(self,perfil):
+    self.__perfil = perfil
+
+  @property
+  def historico(self):
+    return self.__historico
+
+  @endereco.setter
+  def historico(self,historico):
+    self.__historico = historico
+
+  @property
+  def favorito(self):
+    return self.__favorito
+
+  @valor.setter
+  def favorito(self,favorito):
+    self.__favorito = favorito
+    
