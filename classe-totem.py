7197461485f34ba05a8d3dc5faa63984ecf7ebc6
Gabriel Orlow <gabrielorlow@discente.ufg.br>
2023-11-20 18:19:01 -0300

    Update classe-totem.py

--- classe-totem.py
+++ classe-totem.py
@@ -3,4 +3,32 @@ class Totem:
     self.__cinema = None
 
   def __init__(self,filmes,catalogo):
+    self.__filmes = filmes
+    self.__catalogo = catalogo
+
+  @property
+  def cinema(self):
+    return self.__cinema
+
+  @cinema.setter
+  def cinema(self,cinema):
+    self.__cinema = cinema
+
+  @property
+  def filmes(self):
+    return self.__filmes
+
+  @filmes.setter
+  def filmes(self,filmes):
+    self.__filmes = filmes
+
+  @property
+  def catalogo(self):
+    return self.__catalogo
+
+  @catalogo.setter
+  def cinema(self,catalogo):
+    self.__catalogo = catalogo
+
+
     
