b62042b054537262e0786e55d63ea509d3980a7c
matheussfm <134388612+matheussfm@users.noreply.github.com>
2023-11-20 20:01:42 -0300

    classe-diretor.py

--- classe-diretor.py
+++ classe-diretor.py
@@ -1,7 +1,30 @@
 class Diretor: 
+
+  
   def __init__(self, nome, outros_trabalhos, main, biografia):
     self.nome = nome
     self.outros_trabalhos = outros_trabalhos
     self.main = main
     self.biografia = biografia
-    
+
+@property
+def nome(self):
+  return self.__nome
+
+@nome.setter
+def nome(self, nome):
+  self.__nome = nome
+
+@outros_trabalhos.setter
+def outros_trabalhos(self, outros_trabalhos):
+  self.__outros_trabalhos = outros_trabalhos
+
+@property
+def main(self):
+  return self.__main
+
+@main.setter
+def main(self, main):
+  self.__main = main
+
+@property
