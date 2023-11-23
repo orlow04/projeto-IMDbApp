228868fd5a354976c6347cdf2b218802563365d5
Gabriel Orlow <gabrielorlow@discente.ufg.br>
2023-11-23 18:37:25 -0300

    Update classe-usuarioapp.py
    Atualizar os favoritos e criar métodos subscribe e unsubscribe.

--- classe-usuarioapp.py
+++ classe-usuarioapp.py
@@ -1,47 +1,28 @@
 class usuarioApp(Cliente):
-  def __init__(self,tipoCliente,idUsuario,perfil,historico,favorito):
-    super().__init__(tipoCliente)
-    self.__idUsuario = idUsuario
-    self.__perfil = perfil
-    self.__historico = historico
-    self.__favorito = favorito
+  def __init__(self,nome,email,senha,telefone,idioma,idade,localizacao):
+    super().__init__(nome)
+    super().__init__(email)
+    super().__init__(senha)
+    super().__init__(telefone)
+    super().__init__(idioma)
+    self.__idade = idade
+    self.__localizacao = localizacao
 
-@property
-  def idUsuario(self):
-    return self.__idUsuario
 
-  @cpf.setter
-  def idUsuario(self,idUsuario):
-    self.__idUsuario = idUsuario
 
   @property
-  def perfil(self):
-    return self.__perfil
+  def idade(self):
+    return self.__idade
 
-  @empresa.setter
-  def perfil(self,perfil):
-    self.__perfil = perfil
-
-  @property
-  def historico(self):
-    return self.__historico
-
-  @endereco.setter
-  def historico(self,historico):
-    self.__historico = historico
+  @idade.setter
+  def idade(self,idade):
+    self.__idade = idade
 
   @property
-  def favorito(self):
-    return self.__favorito
+  def localizacao(self):
+    return self.__localizacao
 
-  @valor.setter
-  def favorito(self,favorito):
-    self.__favorito = favorito
+  @localizacao.setter
+  def localizacao(self,localizacao):
+    self.__localizacao = localizacao
     
-  #métodos da classe
-
-  def criarPerfil(self, perfil):
-    self.__perfil = perfil
-
-  def criarFav(self, favorito):
-    self.__favorito = favorito
