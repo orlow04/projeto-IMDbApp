5e6280de9c300d190fd7ee8cc366eb7a676f4dda
jvborgex <136815016+jvborgex@users.noreply.github.com>
2023-11-24 19:28:37 -0300

    classe-produtora.py

--- classe-produtora.py
+++ classe-produtora.py
@@ -1,20 +1,38 @@
+#Classe Produtora:
 class Produtora:
-  def __init__(self,nome,pais):
-    self.__nome = nome
-    self.__pais = pais
+    def __init__(self, nome, sede, dono, endereco):
+        self.__nome = nome
+        self.__sede = sede
+        self.__dono = dono
+        self.__endereco = endereco
 
-  @property
-  def nome(self):
-    return self.__nome
+    @property
+    def nome(self):
+        return self.__nome
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
 
-  @nome.setter
-  def nome(self,nome):
-    self.__nome = nome
+    @property
+    def sede(self):
+        return self.__sede
+    @sede.setter
+    def sede(self, sede):
+        self.__sede = sede
 
-  @property
-  def pais(self):
-    return self.__pais
+    @property
+    def dono(self):
+        return self.__dono
+    @dono.setter
+    def dono(self, dono):
+        self.__dono = dono
 
-  @pais.setter
-  def pais(self,pais):
-    self.__pais = pais
+    @property
+    def endereco(self):
+        return self.__endereco
+    @endereco.setter
+    def endereco(self, endereco):
+        self.__endereco = endereco
+
+    def __str__(self):
+        return f'Nome: {self.__nome}\nSede: {self.__sede}\nDono: {self.__dono}\nEndereço: {self.__endereco}'
