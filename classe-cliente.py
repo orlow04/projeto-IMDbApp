05c9fc94e42cb95d21b478be56e7f239ac609c2a
Gabriel Orlow <gabrielorlow@discente.ufg.br>
2023-11-24 09:46:54 -0300

    Update classe-cliente.py

--- classe-cliente.py
+++ classe-cliente.py
@@ -1,49 +1,47 @@
 class Cliente:
     def __init__(self, nome, email, senha, telefone, idiona):
-        self.__nome = nome
-        self.__email = email
-        self.__senha = senha
-        self.__telefone = telefone
-        self.__idioma = idioma
+        self._nome = nome
+        self._email = email
+        self._senha = senha
+        self._telefone = telefone
+        self._idioma = idioma
 
     @property
     def nome(self):
-        return self.__nome
+        return self._nome
 
     @nome.setter
     def nome(self, nome):
-        self.__nome = nome
+        self._nome = nome
 
     @property
     def email(self):
-        return self.__email
+        return self._email
 
     @email.setter
     def email(self, email):
-        self.__email = email
+        self._email = email
 
     @property
     def senha(self):
-        return self.__senha
+        return self._senha
     
     @senha.setter
     def senha(self, senha):
-        self.__senha = senha
+        self._senha = senha
    
     @property
     def telefone(self):
-        return self.__telefone
+        return self._telefone
    
     @telefone.setter
     def (self, telefone):
-        self.__telefone = telefone 
+        self._telefone = telefone 
    
     @property
     def idioma(self):
-        return self.__idioma
+        return self._idioma
     
     @idioma.setter
     def idioma(self, idioma):
-        self.__idioma = idioma
-
-
+        self._idioma = idioma
