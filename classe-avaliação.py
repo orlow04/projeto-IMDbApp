9e5a6f03455f23299ed917d60dd0e3b8c0d5c4c6
Gabriel Orlow <gabrielorlow@discente.ufg.br>
2023-11-24 09:45:49 -0300

    Update classe-avaliação.py

--- classe-avaliação.py
+++ classe-avaliação.py
@@ -1,44 +1,41 @@
 
 class Avaliacao:
     def __init__(self,filme,usuarioApp):
-        self.__filme = None
-        self.__usuarioApp = None    
+        self._filme = None
+        self._usuarioApp = None    
     
     def __init__(self, nota, comentario):
-        self.__nota = nota
-        self.__comentario = comentario
+        self._nota = nota
+        self._comentario = comentario
 
     @property
     def nota(self):
-        return self.__nota
+        return self._nota
 
     @nota.setter
     def nota(self, nota):
-        self.__nota = nota
+        self._nota = nota
 
     @property
     def comentario(self):
-        return self.__comentario
+        return self._comentario
 
     @comentario.setter
     def comentario(self, comentario):
-        self.__comentario = comentario
-
+        self._comentario = comentario
 
     @property
     def filme(self):
-        return self.__filme
+        return self._filme
 
     @filme.setter
     def filme(self, filme):
-        self.__filme = filme
-
+        self._filme = filme
 
     @property
     def usuarioApp(self):
-        return self.__usuarioApp
+        return self._usuarioApp
 
     @usuarioApp.setter
     def usuarioApp(self, usuarioApp):
-        self.__usuarioApp = usuarioApp
-
+        self._usuarioApp = usuarioApp
