fab179603d19ea237bafc715305aa042a475c3d3
jvborgex <136815016+jvborgex@users.noreply.github.com>
2023-11-24 20:24:14 -0300

    classe-ingresso.py

--- classe-ingresso.py
+++ classe-ingresso.py
@@ -1,64 +1,66 @@
 class Ingresso:
-  def __init__(self,vendido = True,horario,poltrona,sala,identificador):
-    self._vendido = vendido
-    self._horario = horario
-    self._poltrona = poltrona
-    self._sala = sala
-    self._identificador = identificador
-    self._avaliacao = None
-    self._usuarioApp = None
+    def __init__(self, horario, poltrona, sala, identificador, vendido=True):
+        self._vendido = vendido
+        self._horario = horario
+        self._poltrona = poltrona
+        self._sala = sala
+        self._identificador = identificador
+        self._avaliacao = None
+        self._usuarioApp = None
 
+    @property
+    def avaliacao(self):
+        return self._avaliacao
 
-  @property
-  def avaliacao(self):
-    return self._avaliacao
+    @avaliacao.setter
+    def avaliacao(self, avaliacao):
+        self._avaliacao = avaliacao
 
-  @avaliacao.setter
-  def avaliacao(self,avaliacao):
-    self._avaliacao = avaliacao
+    @property
+    def usuarioApp(self):
+        return self._usuarioApp
 
-  @property
-  def usuarioApp(self):
-      return self._usuarioApp
+    @usuarioApp.setter
+    def usuarioApp(self, usuarioApp):
+        self._usuarioApp = usuarioApp
 
-  @usuarioApp.setter
-  def usuarioApp(self, usuarioApp):
-      self._usuarioApp = usuarioApp
-    
-  def get_vendido(self):
-    return self._vendido
+    def get_vendido(self):
+        return self._vendido
 
-  def set_vendido(self,vendido):
-    self._vendido = vendido
+    def set_vendido(self, vendido):
+        self._vendido = vendido
 
-  @property
-  def horario(self):
-    return self._horario
+    @property
+    def horario(self):
+        return self._horario
 
-  @horario.setter
-  def horario(self,horario):
-    self._horario = horario
+    @horario.setter
+    def horario(self, horario):
+        self._horario = horario
 
-  @property
-  def poltrona(self):
-    return self._poltrona
+    @property
+    def poltrona(self):
+        return self._poltrona
 
-  @poltrona.setter
-  def poltrona(self,poltrona):
-    self._poltrona = poltrona
+    @poltrona.setter
+    def poltrona(self, poltrona):
+        self._poltrona = poltrona
 
-  @property
-  def sala(self):
-    return self._sala
+    @property
+    def sala(self):
+        return self._sala
 
-  @sala.setter
-  def sala(self,sala):
-    self._sala = sala
+    @sala.setter
+    def sala(self, sala):
+        self._sala = sala
 
-  @property
-  def identificador(self):
-    return self._identificador
+    @property
+    def identificador(self):
+        return self._identificador
 
-  @identificador.setter
-  def identificador(self,identificador)
-    self._identificador = identificador
+    @identificador.setter
+    def identificador(self, identificador):
+        self._identificador = identificador
+
+    def __str__(self):
+      return f"Horário: {self.horario}\nPoltrona: {self.poltrona}\nSala: {self.sala}\nIdentificador: {self.identificador}\nVendido: {self._vendido}\nAvaliação: {self._avaliacao}\nUsuário: {self._usuarioApp}"
