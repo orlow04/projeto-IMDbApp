52a93c739361d5ab5690b39cea4affa30de3676f
Gabriel Orlow <gabrielorlow@discente.ufg.br>
2023-11-24 09:47:52 -0300

    Update classe-empresa.py

--- classe-empresa.py
+++ classe-empresa.py
@@ -1,52 +1,50 @@
 class Empresa:
   def __init__(self,Proprietario):
-    self.__Proprietario = None
+    self._Proprietario = None
 
   def __init__(self,cnpj,franquia,CEP,numero,complemento):
-    self.__cnpj = cnpj
-    self.__franquia = franquia
-    self.__CEP = CEP
-    self.__numero = numero
-    self.__complemento = complemento
+    self._cnpj = cnpj
+    self._franquia = franquia
+    self._CEP = CEP
+    self._numero = numero
+    self._complemento = complemento
 
   @property
   def Proprietario(self):
-    return self.__Proprietario
+    return self._Proprietario
 
   @Proprietario.setter
   def Propietario(self,Propietario):
-    self.__Propietario = Propietario
+    self._Propietario = Propietario
 
   @property
   def cnpj(self):
-    return self.__cnpj
+    return self._cnpj
 
   @cnpj.setter
   def cnpj(self,cnpj):
-    self.__cnpj = cnpj
+    self._cnpj = cnpj
 
   @property
   def CEP(self):
-    return self.__CEP
+    return self._CEP
     
   @CEP.setter
   def CEP(self,CEP):
-    self.__CEP = CEP
+    self._CEP = CEP
 
   @property
   def numero(self):
-    return self.__numero
+    return self._numero
 
   @numero.setter
   def numero(self,numero):
-    self.__numero = numero
+    self._numero = numero
 
   @property
   def complemento(self):
-    return self.__complemento
+    return self._complemento
 
   @complemento.setter
   def complemento(self,complemento):
-    self.__complemento = complemento
-
-
+    self._complemento = complemento
