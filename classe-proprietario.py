e3226fb658ce031fb5ee1645829ce62260e8d4d9
jvborgex <136815016+jvborgex@users.noreply.github.com>
2023-11-24 21:58:05 -0300

    classe-proprietario.py

--- classe-proprietario.py
+++ classe-proprietario.py
@@ -1,28 +1,35 @@
+#Classe Propietario: Ok
 class Proprietario(Cliente):
-  def __init__(self,nome,email,senha,telefone,idioma,cpf,numeroFranquia):
-    super().__init__(nome,email,senha,telefone,idioma)
-    self._cpf = cpf
-    self._numeroFranquia = numeroFranquia
-    self.empresa = None
+    def __init__(self, nome, email, senha, telefone, idioma, cpf, numeroFranquia):
+        super().__init__(nome, email, senha, telefone, idioma)
+        self._cpf = cpf
+        self._numeroFranquia = numeroFranquia
+        self.empresa = None
 
-  def checaFranquia(self):
-    if self.empresa.get_franquia():
-      print("A empresa é franquia")
-    else:
-      print("A empresa não é franquia")
-      
-  @property
-  def cpf(self):
-    return self._cpf
+    def checaFranquia(self):
+        if self.empresa.get_franquia():
+            print("A empresa é franquia")
+        else:
+            print("A empresa não é franquia")
 
-  @cpf.setter
-  def cpf(self,cpf):
-    self._cpf = cpf
+    @property
+    def cpf(self):
+        return self._cpf
 
-  @property
-  def numeroFranquia(self):
-    return self._numeroFranquia
+    @cpf.setter
+    def cpf(self, cpf):
+        self._cpf = cpf
 
-  @numeroFranquia.setter
-  def numeroFranquia(self,numeroFranquia):
-    self._numeroFranquia = numeroFranquia
+    @property
+    def numeroFranquia(self):
+        return self._numeroFranquia
+    @numeroFranquia.setter
+    def numeroFranquia(self, numeroFranquia):
+        self._numeroFranquia = numeroFranquia
+
+    def associarEmpresa(self, empresa):
+        self.empresa = empresa.nome
+        empresa.associarProprietario(self)
+
+    def __str__(self):
+        return f"Nome: {self.nome}\nEmail: {self.email}\nTelefone: {self.telefone}\nCPF: {self.cpf}\nNúmero da Franquia: {self.numeroFranquia}\nEmpresa: {self.empresa}"
